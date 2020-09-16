577370169e889afef759572dd3857ca7f7820bda
mabasasi <aoe317293@yahoo.co.jp>
2020-09-16 18:00:43 +0900

    [add]Home を Vuetify仕様で実装してみた
    ・layout も使用

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,8 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return Inertia\Inertia::render('Home');
+    // return view('welcome');
 });
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
