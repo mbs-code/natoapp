96190f304d7d1e6cc862eeb93a2d54aad39210b6
mabasasi <aoe317293@yahoo.co.jp>
2020-09-20 16:55:00 +0900

    [mod] キャッシュ削除のためのバージョニングを実装
    ・とりあえずjsだけ
    ・product時は解除したい

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,9 +4,6 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
-        @if (app()->environment() !== 'production')
-            <meta http-equiv="Cache-Control" content="no-cache">
-        @endif
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
@@ -20,7 +17,7 @@
 
         <!-- Scripts -->
         <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.26.0/moment.min.js"></script>
-        <script src="{{ asset('js/app.js') }}" defer></script>
+        <script src="{{ asset('js/app.js').'?t='.now()->timestamp }}" defer></script>
     </head>
     <body class="font-sans antialiased">
         @inertia
