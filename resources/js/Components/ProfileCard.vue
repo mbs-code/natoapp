e60182fbecc6d41d4b967aa66c905e963b2d736d
mabasasi <aoe317293@yahoo.co.jp>
2020-10-05 03:10:30 +0900

    [mod] profile card から button を削除

--- resources/js/Components/ProfileCard.vue
+++ resources/js/Components/ProfileCard.vue
@@ -1,11 +1,10 @@
 <template>
   <v-hover v-slot:default="{ hover }">
-    <!-- 非 buttonMode の時はリンクが有効 -->
     <v-card
       class="mx-auto"
-      :elevation="hover && !buttonMode ? 12 : 2"
-      :[cardHref]="route('profiles.show', { id: profile.id })"
-      @[cardClick].stop.prevent="$inertia.visit(route('profiles.show', { id: profile.id }))"
+      :elevation="hover ? 12 : 2"
+      :href="route('profiles.show', { id: profile.id })"
+      @click.stop.prevent="$inertia.visit(route('profiles.show', { id: profile.id }))"
     >
       <v-list-item>
         <v-list-item-avatar color="grey" size="64">
@@ -28,50 +27,6 @@
             </v-chip>
           </v-list-item-subtitle>
         </v-list-item-content>
-
-        <!-- draw when buttonMode -->
-        <v-list-item-action v-if="buttonMode">
-          <v-row>
-            <v-btn
-              height="64"
-              color="info"
-              outlined
-              class="mx-1"
-              :href="route('profiles.show', { id: profile.id })"
-              @click.stop.prevent="$inertia.visit(route('profiles.show', { id: profile.id }))"
-            >
-              <v-icon>mdi-arrow-right-bold-box-outline</v-icon>
-            </v-btn>
-            <v-btn
-              height="64"
-              color="success"
-              outlined
-              class="mx-1"
-              :href="route('profiles.edit', { id: profile.id })"
-              @click.stop.prevent="$inertia.visit(route('profiles.edit', { id: profile.id }))"
-            >
-              <v-icon>mdi-pencil</v-icon>
-            </v-btn>
-            <ConfirmDialog @ok="$inertia.delete(route('profiles.destroy', { id: profile.id }))">
-              <template v-slot:activator="{ attrs, on }">
-                <v-btn
-                  height="64"
-                  color="error"
-                  outlined
-                  class="mx-1"
-                  v-bind="attrs"
-                  v-on="on"
-                >
-                  <v-icon>mdi-delete</v-icon>
-                </v-btn>
-              </template>
-              <template v-slot:text>
-                「{{ profile.name }}」を削除しますか？
-              </template>
-            </ConfirmDialog>
-          </v-row>
-        </v-list-item-action>
-        <!-- end draw when buttonMode -->
       </v-list-item>
 
       <!-- separator -->
@@ -131,12 +86,9 @@
 </template>
 
 <script>
-import ConfirmDialog from '@/Components/CommonParts/ConfirmDialog'
 import StringFormatter from '@/Mixins/StringFormatter'
 
 export default {
-  components: { ConfirmDialog },
-
   filters: {
     toYoutubeChannelLink: function (channelId) {
       return 'https://www.youtube.com/channel/' + channelId
@@ -151,20 +103,6 @@ export default {
     profile: {
       type: Object,
       default: () => {},
-    },
-    buttonMode: {
-      // true で button, false で card link
-      type: Boolean,
-      default: false,
-    },
-  },
-
-  computed: {
-    cardHref: function () {
-      return this.buttonMode ? null : 'href'
-    },
-    cardClick: function () {
-      return this.buttonMode ? null : 'click'
     },
   },
 }
