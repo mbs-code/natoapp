417e98759917869cd67f811f4be8bb111be0bf06
mabasasi <aoe317293@yahoo.co.jp>
2020-10-12 17:04:50 +0900

    [mod] base model を abstract 化して指定ミスを防ぐ

--- app/Models/BaseModel.php
+++ app/Models/BaseModel.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-class BaseModel extends Model
+abstract class BaseModel extends Model
 {
   use HasFactory;
 
