6adcc805f1d8645d5c61815e6d63ca73ea651982
mabasasi <aoe317293@yahoo.co.jp>
2020-10-31 03:15:24 +0900

    [mod] event log のフォーマット統一とリファクタリング
    ・共通事項の関数化
    ・ネスト数に応じて括弧を統一した

--- app/lib/Tasks/Utils/GeneralEvents.php
+++ app/lib/Tasks/Utils/GeneralEvents.php
@@ -5,82 +5,128 @@ namespace App\Lib\Tasks\Utils;
 
 class GeneralEvents
 {
-    public static function arrayTaskEvents(string $eventName = null) {
+    public static function arrayTaskEvents(string $eventName = null)
+    {
         $e = self::core(true);
 
         $e->put('beforeOuterLoop', function ($e) use ($eventName) {
+            $pref = $eventName ?? 'Task';
             $total = self::wrapCount($e->execProps);
-            $prefStr = $eventName ?? 'Task';
-            $mes = "{$prefStr} (length: {$e->outerLength}, total: {$total})";
-            logger()->notice($mes);
+            logger()->notice("{$pref} (length: {$e->outerLength}, total: {$total})");
         });
 
         /// ////////////////////////////////////////
 
         $e->put('outerLooped', function ($e) use ($eventName) {
-            $pref = $eventName ? $eventName : 'Outer loop';
+            $pref = $eventName ?? 'Task';
             $time = number_format(microtime(true) - $e->startTimestamp, 2);
-            $rate = $e->outerTotal !== 0
-                ? number_format((($e->outerSuccess + $e->outerSkip) / $e->outerTotal) * 100, 2)
-                : number_format(100, 2);
-            $stat = "{$rate}%, {$e->outerSuccess}+{$e->outerSkip}/{$e->outerTotal}, skip: {$e->outerSkip}, throw: {$e->outerThrow}";
-            $mes = "Finish! {$pref} {$time}sec ({$stat})";
-            logger()->notice($mes);
+            $stat = self::rateStatString($e, 'outer');
+            logger()->notice("Finish! {$pref} {$time}sec ({$stat})");
         });
 
         return $e;
     }
 
-    public static function seriesArrayTaskEvents(string $eventName = null) {
+    public static function seriesArrayTaskEvents(string $eventName = null)
+    {
         $e = self::core();
 
         $e->put('beforeSeriesLoop', function ($e) use ($eventName) {
-            $prefStr = $eventName ?? 'Series loop';
-            $mes = "{$prefStr} (length: {$e->seriesLength})";
-            logger()->notice($mes);
+            $pref = $eventName ?? 'Series loop';
+            logger()->notice("{$pref} (length: {$e->seriesLength})");
         });
 
         $e->put('beforeOuterLoop', function ($e) {
+            $prog = self::progresString($e->seriesIndex, $e->seriesLength, true, '{', '}');
+            $task = self::wrapString($e->outerProps, 60);
             $total = self::wrapCount($e->execProps);
-            $task = $e->outerProps;
-            $mes = "<{$e->seriesIndex}/{$e->seriesLength}> Task {$task} (length: {$e->outerLength}, total: {$total})";
-            logger()->info($mes);
+            logger()->info("{$prog} Task {$task} (length: {$e->outerLength}, total: {$total})");
         });
 
         /// ////////////////////////////////////////
 
         $e->put('outerLooped', function ($e) {
-            $task = $e->outerProps;
-            $rate = $e->outerTotal !== 0
-                ? number_format((($e->outerSuccess + $e->outerSkip) / $e->outerTotal) * 100, 2)
-                : number_format(100, 2);
-            $stat = "{$rate}%, {$e->outerSuccess}+{$e->outerSkip}/{$e->outerTotal}, skip: {$e->outerSkip}, throw: {$e->outerThrow}";
-            $mes = "<{$e->seriesIndex}/{$e->seriesLength}> Task finish {$task} ({$stat})";
-            logger()->info($mes);
+            $prog = self::progresString($e->seriesIndex, $e->seriesLength, true, '{', '}');
+            $task = self::wrapString($e->outerProps, 60);
+            $stat = self::rateStatString($e, 'outer');
+            logger()->info("{$prog} Task finish {$task} ({$stat})");
         });
 
         $e->put('seriesLooped', function ($e) use ($eventName) {
-            $pref = $eventName ? $eventName : 'Series loop';
+            $pref = $eventName ?? 'Series loop';
             $time = number_format(microtime(true) - $e->startTimestamp, 2);
-            $rate = $e->seriesTotal !== 0
-                ? number_format((($e->seriesSuccess + $e->seriesSkip) / $e->seriesTotal) * 100, 2)
-                : number_format(100, 2);
-            $stat = "{$rate}%, {$e->seriesSuccess}+{$e->seriesSkip}/{$e->seriesTotal}, skip: {$e->seriesSkip}, throw: {$e->seriesThrow}";
-            $mes = "Finish! {$pref} {$time}sec ({$stat})";
-            logger()->notice($mes);
+            $stat = self::rateStatString($e, 'series');
+            logger()->notice("Finish! {$pref} {$time}sec ({$stat})");
         });
 
         return $e;
     }
 
     /// ////////////////////////////////////////////////////////////
+    /// ////////////////////////////////////////////////////////////
+    /// ////////////////////////////////////////////////////////////
 
-    private static function wrapResponse($res)
+    private static function core(bool $showDenominator = false)
     {
-        if (is_array($res)) {
-            return json_encode($res, JSON_UNESCAPED_UNICODE);
+        $e = collect();
+
+        $e->put('beforeInnerLoop', function ($e) use ($showDenominator) {
+            $prog = self::progresString($e->outerIndex, $e->outerLength, $showDenominator, '<', '>');
+            logger()->info("{$prog} Handle loop (length: {$e->innerLength})");
+        });
+
+        $e->put('fetched', function ($e) use ($showDenominator) {
+            $prog = self::progresString($e->outerIndex, $e->outerLength, $showDenominator, '<', '>');
+            $count = self::wrapCount($e->fetchResponse);
+            logger()->info("{$prog} Fetch {$count} items");
+        });
+
+        /// ////////////////////////////////////////
+
+        $e->put('innerSuccess', function ($e) {
+            $prog = self::progresString($e->innerIndex, $e->innerLength, true, '[', ']');
+            $method = $e->handleMethod ?? 'success';
+            $res = self::wrapString($e->handleResponse, 60);
+            logger()->debug("{$prog} {$method}: {$e->innerKey} => {$res}");
+        });
+        $e->put('innerSkip', function ($e) {
+            $prog = self::progresString($e->innerIndex, $e->innerLength, true, '[', ']');
+            $method = 'skip';
+            logger()->debug("{$prog} {$method}: {$e->innerKey}");
+        });
+        $e->put('innerException', function ($e) {
+            $prog = self::progresString($e->innerIndex, $e->innerLength, true, '[', ']');
+            $method = 'throw';
+            logger()->error("{$prog} {$method}: {$e->innerKey}");
+            logger()->error($e->exception); // 例外も吐いとく
+        });
+
+        /// ////////////////////////////////////////
+
+        $e->put('innerLooped', function ($e) use ($showDenominator) {
+            $prog = self::progresString($e->outerIndex, $e->outerLength, $showDenominator, '<', '>');
+            $stat = self::rateStatString($e, 'outer');
+            logger()->info("{$prog} Handle loop finish ({$stat})");
+        });
+
+        return $e;
+    }
+
+    private static function wrapString($res, $limit = 0)
+    {
+        $str = $res;
+        if (is_iterable($res)) {
+            $str = json_encode($res, JSON_UNESCAPED_UNICODE);
         }
-        return $res;
+
+        // 文字数制限
+        if ($limit > 0) {
+            $len = strlen($str);
+            if ($len > $limit - 3) {
+                $str = substr($str, 0, $limit).'...';
+            }
+        }
+        return $str;
     }
 
     private static function wrapCount($res)
@@ -91,63 +137,24 @@ class GeneralEvents
         return $res ? 1 : 0;
     }
 
-    private static function progresString($numer, $denom, $showDenominator = true)
+    private static function rateStatString($e, $prefix)
     {
-        return $showDenominator ? "<{$numer}/{$denom}>" : "{{$numer}}";
+        // 項目が多すぎるから prefix から導出（融通が効かない弱点）
+        $total = $e->get($prefix.'Total');
+        $success = $e->get($prefix.'Success');
+        $skip = $e->get($prefix.'Skip');
+        $throw = $e->get($prefix.'Throw');
+
+        $rate = $total !== 0
+            ? number_format((($success + $skip) / $total) * 100, 2)
+            : number_format(100, 2);
+        $stat = "{$rate}%, {$success}+{$skip}/{$total}, skip: {$skip}, throw: {$throw}";
+        return $stat;
     }
 
-    private static function core(bool $showDenominator = false)
+    private static function progresString($numer, $denom, $showDenominator = true, $bracket = '(', $endBracket = ')')
     {
-        $e = collect();
-
-        $e->put('beforeInnerLoop', function ($e) use ($showDenominator) {
-            $progStr = self::progresString($e->outerIndex, $e->outerLength, $showDenominator);
-            $mes = "{$progStr} Handle loop (length: {$e->innerLength})";
-            logger()->info($mes);
-        });
-
-        /// ////////////////////////////////////////
-
-        $e->put('fetched', function ($e) use ($showDenominator) {
-            $progStr = self::progresString($e->outerIndex, $e->outerLength, $showDenominator);
-            $count = self::wrapCount($e->fetchResponse);
-            $mes = "{$progStr} Fetch {$count} items";
-            logger()->info($mes);
-        });
-
-        $e->put('innerSuccess', function ($e) use ($showDenominator) {
-            $progStr = self::progresString($e->innerIndex, $e->innerLength, $showDenominator);
-            $methodStr = $e->handleMethod ?? 'success';
-            $res = self::wrapResponse($e->handleResponse);
-            $mes = "{$progStr} {$methodStr}: {$e->innerKey} => {$res}";
-            logger()->debug($mes);
-        });
-        $e->put('innerSkip', function ($e) use ($showDenominator) {
-            $progStr = self::progresString($e->innerIndex, $e->innerLength, $showDenominator);
-            $methodStr = 'skip';
-            $mes = "{$progStr}  {$methodStr}: {$e->innerKey}";
-            logger()->debug($mes);
-        });
-        $e->put('innerException', function ($e) use ($showDenominator) {
-            $progStr = self::progresString($e->innerIndex, $e->innerLength, $showDenominator);
-            $methodStr = 'throw';
-            $mes = "{$progStr} {$methodStr}: {$e->innerKey}";
-            logger()->error($mes);
-            logger()->error($e->exception); // 例外も吐いとく
-        });
-
-        /// ////////////////////////////////////////
-
-        $e->put('innerLooped', function ($e) use ($showDenominator) {
-            $progStr = self::progresString($e->outerIndex, $e->outerLength, $showDenominator);
-            $rate = $e->innerLength !== 0
-                ? number_format((($e->innerSuccess + $e->innerSkip) / $e->innerLength) * 100, 2)
-                : number_format(100, 2);
-            $stat = "{$rate}%, {$e->innerSuccess}+{$e->innerSkip}/{$e->innerLength}, skip: {$e->innerSkip}, throw: {$e->innerThrow}";
-            $mes = "{$progStr} Handle loop finish ({$stat})";
-            logger()->info($mes);
-        });
-
-        return $e;
+        $val = $showDenominator ? "{$numer}/{$denom}" : $numer;
+        return $bracket.$val.$endBracket;
     }
 }
